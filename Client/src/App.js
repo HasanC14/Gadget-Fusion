9d3472d114915831285210a620d85000808ea365
HasanC14 <chowdhuryh999@gmail.com>
2023-08-04 13:35:59 +0600

    server updated

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
-
+import { createContext } from "react";
+export const PRODUCT_CONTEXT = createContext();
 function App() {
   return (
     <div>
